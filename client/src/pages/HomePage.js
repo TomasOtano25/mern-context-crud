69169835bd3fe59d4ba84b965e4a3c3d8afd50ef
tomasotano25 <tomasotano25@gmail.com>
2022-04-10 14:50:30 -0400

    add context

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,21 @@
+import { usePosts } from '../context/postContext'
+import { Link } from 'react-router-dom'
+
 export function HomePage()
 {
+    const { posts, setPosts } = usePosts()
+
+
     return (
-        <h1>HomePage</h1>
+        <div>
+
+            <h1>HomePage</h1>
+
+            <Link to="/new" className='text-blue-100 block'>Go to new</Link>
+
+            <button className='bg-red-100' onClick={() => setPosts([1, 2, 3])}>
+                add
+            </button>
+        </div>
     )
 }
